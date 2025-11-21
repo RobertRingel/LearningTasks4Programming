b2da5b91c755cff169adb9de4774b16685778c1d
Robert Ringel <RobertRingel@users.noreply.github.com>
2025-11-21 12:43:36 +0100

    Update Person.java

--- Java/06_ExceptionHandling/Person.java
+++ Java/06_ExceptionHandling/Person.java
@@ -1,17 +1,10 @@
 package org.htwd.pool.o6;
 
-import java.util.regex.Pattern;
-
 public class Person {
     private String name;
     private final String birthDay;
 
-    public Person(String name, String bday) throws IllegalArgumentException {
-        if (name == null || name=="")
-            throw new IllegalArgumentException("Invalid parameter for name");
-        // Pattern.matches: perform basic date format check: DD/MM/YYYY
-        if (bday == null || Pattern.matches("^\\d{2}[/-]\\d{2}[/-][012]\\d{3}$", bday) == false)
-            throw new IllegalArgumentException("Invalid parameter for bday");
+    public Person(String name, String bday) {
         this.name = name;
         this.birthDay = bday;
     }
@@ -27,12 +20,7 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        try {
-            Person max = new Person("Max", "07.11/2002");
-            System.out.println(max);
-        } catch (IllegalArgumentException e) {
-            System.out.println("There is a problem: "+e.getMessage());
-        }
-        System.out.println("regular end of the program");
+        Person max = new Person("Max", "07.11/2002");
+        System.out.println(max);
     }
 }
